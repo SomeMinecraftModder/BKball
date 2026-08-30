133c861f6cf2123ddb85743d43fd23018d75e22d
24oulia <staff@webgarbage.frt>
2026-08-30 17:53:06 +0200

    Formatted JS file

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function showKeyPress(evt) {
 }
 
 
-function doThing(){
-// test but not used
-var segment = document.getElementById("circleAnimate");
-segment.setAttribute("from", old_x+" "+old_y);
-segmenty.setAttribute("to", x+" "+y);
-segmenty.beginElement();
+function doThing() {
+    // test but not used
+    var segment = document.getElementById("circleAnimate");
+    segment.setAttribute("from", old_x + " " + old_y);
+    segmenty.setAttribute("to", x + " " + y);
+    segmenty.beginElement();
 }
 
 
@@ -47,92 +47,91 @@ var old_y = y
 
 function changeSegment(name) {
 
-var segment = document.getElementById(name + "1");
-segment.setAttribute("values", old_x+";"+x);
-segment.beginElement();
-var segmenty = document.getElementById(name + "2");
-segmenty.setAttribute("values", old_y+";"+y);
-segmenty.beginElement();
-console.log(name)
+    var segment = document.getElementById(name + "1");
+    segment.setAttribute("values", old_x + ";" + x);
+    segment.beginElement();
+    var segmenty = document.getElementById(name + "2");
+    segmenty.setAttribute("values", old_y + ";" + y);
+    segmenty.beginElement();
+    console.log(name)
 }
 
-function actualize(){
-old_x = x
-x = ((d**2)-(r**2)+(R**2))/(2*d)
+function actualize() {
+    old_x = x
+    x = ((d ** 2) - (r ** 2) + (R ** 2)) / (2 * d)
 
-old_y = y
-var a = (1/d) * ((-d+r-R)*(-d-r+R)*(-d+r+R)*(d+r+R))**(1/2)
-y = a/2
+    old_y = y
+    var a = (1 / d) * ((-d + r - R) * (-d - r + R) * (-d + r + R) * (d + r + R)) ** (1 / 2)
+    y = a / 2
 
-if (x < 5) {
-x = 5
-}
-else if (x > 135){
-x = 135
-}
+    if (x < 5) {
+        x = 5
+    } else if (x > 135) {
+        x = 135
+    }
 
-// var circle = document.getElementById("circlexy");
-// circle.setAttribute("values", "translate("+x+","+ y+")");
-
-
-/*
-var newAnimatex = document.createElement("animate");
-var currentAnimatex = document.getElementById(name);
-currentAnimatex.appendChild(newAnimatex);
-newAnimatex.setAttribute("dur", " 0.2s")
-// newAnimatex.setAttribute("fill", "freeze")
-newAnimatex.setAttribute("begin", "0s")
-newAnimatex.setAttribute("attributeName", "x1")
-newAnimatex.setAttribute("values", x)
-var newAnimatey = document.createElement("animate", {attributeName:"y1", values:y, dur:" 0.2s", fill:"freeze"});
-var currentAnimatey = document.getElementById(name);
-currentAnimatey.appendChild(newAnimatey);
-newAnimatey.setAttribute("dur", " 0.2s")
-// newAnimatey.setAttribute("fill", "freeze")
-newAnimatey.setAttribute("attributeName", "y1")
-newAnimatey.setAttribute("values", y) */
-
-changeSegment("right1")
-changeSegment("right2")
-changeSegment("right3")
-changeSegment("left1")
-changeSegment("left2")
-changeSegment("left3")
+    // var circle = document.getElementById("circlexy");
+    // circle.setAttribute("values", "translate("+x+","+ y+")");
 
 
+    /*
+    var newAnimatex = document.createElement("animate");
+    var currentAnimatex = document.getElementById(name);
+    currentAnimatex.appendChild(newAnimatex);
+    newAnimatex.setAttribute("dur", " 0.2s")
+    // newAnimatex.setAttribute("fill", "freeze")
+    newAnimatex.setAttribute("begin", "0s")
+    newAnimatex.setAttribute("attributeName", "x1")
+    newAnimatex.setAttribute("values", x)
+    var newAnimatey = document.createElement("animate", {attributeName:"y1", values:y, dur:" 0.2s", fill:"freeze"});
+    var currentAnimatey = document.getElementById(name);
+    currentAnimatey.appendChild(newAnimatey);
+    newAnimatey.setAttribute("dur", " 0.2s")
+    // newAnimatey.setAttribute("fill", "freeze")
+    newAnimatey.setAttribute("attributeName", "y1")
+    newAnimatey.setAttribute("values", y) */
 
-function boardHole(coordonates, radius) {
-var newAnimatex = document.createElement("animate");
-var currentAnimatex = document.getElementById(name);
-currentAnimatex.appendChild(newAnimatex);
-newAnimatex.setAttribute("dur", " 0.2s")
-}
+    changeSegment("right1")
+    changeSegment("right2")
+    changeSegment("right3")
+    changeSegment("left1")
+    changeSegment("left2")
+    changeSegment("left3")
 
-// var segment = document.getElementById("right11");
-// segment.setAttribute("x1", x);
-// var segment = document.getElementById("right12");
-// segment.setAttribute("y1", y);
-/*
-var right = document.getElementById("right2");
-right.setAttribute("x1", x);
-right.setAttribute("y1", y);
-var right = document.getElementById("right3");
-right.setAttribute("x1", x);
-right.setAttribute("y1", y);
-var left = document.getElementById("left1");
-left.setAttribute("x1", x);
-left.setAttribute("y1", y);
-var left = document.getElementById("left2");
-left.setAttribute("x1", x);
-left.setAttribute("y1", y);
-var left = document.getElementById("left3");
-left.setAttribute("x1", x);
-left.setAttribute("y1", y);*/
-var segment = document.getElementById("circleAnimate");
-segment.setAttribute("from", old_x+" "+old_y);
-segment.setAttribute("to", x+" "+y);
-segment.beginElement();
-// console.log('circle.setAttribute("transform", "translate('+x+','+ y+')")')
+
+
+    function boardHole(coordonates, radius) {
+        var newAnimatex = document.createElement("animate");
+        var currentAnimatex = document.getElementById(name);
+        currentAnimatex.appendChild(newAnimatex);
+        newAnimatex.setAttribute("dur", " 0.2s")
+    }
+
+    // var segment = document.getElementById("right11");
+    // segment.setAttribute("x1", x);
+    // var segment = document.getElementById("right12");
+    // segment.setAttribute("y1", y);
+    /*
+    var right = document.getElementById("right2");
+    right.setAttribute("x1", x);
+    right.setAttribute("y1", y);
+    var right = document.getElementById("right3");
+    right.setAttribute("x1", x);
+    right.setAttribute("y1", y);
+    var left = document.getElementById("left1");
+    left.setAttribute("x1", x);
+    left.setAttribute("y1", y);
+    var left = document.getElementById("left2");
+    left.setAttribute("x1", x);
+    left.setAttribute("y1", y);
+    var left = document.getElementById("left3");
+    left.setAttribute("x1", x);
+    left.setAttribute("y1", y);*/
+    var segment = document.getElementById("circleAnimate");
+    segment.setAttribute("from", old_x + " " + old_y);
+    segment.setAttribute("to", x + " " + y);
+    segment.beginElement();
+    // console.log('circle.setAttribute("transform", "translate('+x+','+ y+')")')
 
 
 }
